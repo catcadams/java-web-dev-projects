71c52ca8560606a5db289567a9e051a8f4d0b2fc
catcadams <catceciliaadams@gmail.com>
2024-09-18 09:15:29 -0500

    java data types exercises

---
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/MilesPerGallon.java
@@ -0,0 +1,18 @@
+package org.launchcode;
+
+import java.util.Scanner;
+
+public class MilesPerGallon {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("How many miles have you driven: ");
+        float miles = input.nextFloat();
+
+        System.out.println("How many gallons of gas have you used: ");
+        float gallonsOfGas = input.nextFloat();
+
+        float milesPerGallon = miles/gallonsOfGas;
+        System.out.println("MPG: " + milesPerGallon);
+    }
+}
---
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/RectangleArea.java
@@ -0,0 +1,14 @@
+package org.launchcode;
+
+import java.util.Scanner;
+
+public class RectangleArea {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is the length of the rectangle: ");
+        Integer rectangleLength = input.nextInt();
+        System.out.println("What is the width of the rectangle: ");
+        Integer rectangleWidth = input.nextInt();
+        System.out.println("Area: " + (rectangleWidth*rectangleLength));
+    }
+}
