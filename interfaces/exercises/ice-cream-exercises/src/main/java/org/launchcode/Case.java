711c70a02c798e973c413da0cd6b238ede48294a
catcadams <catceciliaadams@gmail.com>
2024-11-07 17:12:01 -0600

    interfaces exercises

--- interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Case.java
+++ interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Case.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 public class Case {
     private ArrayList<Flavor> flavors = new ArrayList<>();
     private ArrayList<Cone> cones = new ArrayList<>();
+    private ArrayList<Topping> toppings = new ArrayList<>();
 
     public Case(){
         Cone cone1 = new Cone("Waffle", 1.25, new ArrayList<>(Arrays.asList( "gluten")));
@@ -27,6 +28,18 @@ public class Case {
         flavors.add(flavor3);
         flavors.add(flavor4);
         flavors.add(flavor5);
+
+        Topping topping1 = new Topping("Sprinkles", .10, new ArrayList<>(Arrays.asList("none")));
+        Topping topping2 = new Topping("Cookie Dough", .50, new ArrayList<>(Arrays.asList("gluten, dairy")));
+        Topping topping3 = new Topping("Chocolate Sauce", .25, new ArrayList<>(Arrays.asList("none")));
+        Topping topping4 = new Topping("Granola", .25, new ArrayList<>(Arrays.asList("gluten")));
+        Topping topping5 = new Topping("Andes Mints", .75, new ArrayList<>(Arrays.asList("dairy")));
+        toppings.add(topping1);
+        toppings.add(topping2);
+        toppings.add(topping3);
+        toppings.add(topping4);
+        toppings.add(topping5);
+
     }
 
     public ArrayList<Flavor> getFlavors() {
@@ -37,11 +50,19 @@ public class Case {
         return cones;
     }
 
+    public ArrayList<Topping> getToppings() {
+        return toppings;
+    }
+
     public void setFlavors(ArrayList<Flavor> flavors) {
         this.flavors = flavors;
     }
 
     public void setCones(ArrayList<Cone> cones) {
         this.cones = cones;
+    }
+
+    public void setToppings(ArrayList<Topping> toppings) {
+        this.toppings = toppings;
     }
 }
--- interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
+++ interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
@@ -1,12 +1,26 @@
 package org.launchcode;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
         Case menu = new Case();
         ArrayList<Flavor> flavors = menu.getFlavors();
         ArrayList<Cone> cones = menu.getCones();
+        ArrayList<Topping> toppings = menu.getToppings();
+
+        flavors.sort(new FlavorComparator().reversed());
+        cones.sort(new ConeComparator());
+        toppings.sort(new ToppingsComparator());
+
+        for (Flavor oneFlavor: flavors) {
+            System.out.println(oneFlavor.getAllergens());
+        }
+
+        System.out.println(cones);
+
+        System.out.println(toppings);
 
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name' field.
 
