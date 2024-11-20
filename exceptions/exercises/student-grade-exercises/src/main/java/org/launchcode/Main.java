8cf35d964c425762ddf09fa4bf47715313c6a447
catcadams <catceciliaadams@gmail.com>
2024-11-20 17:35:38 -0600

    exceptions examples and exercise

--- exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
         // Test out your Divide() function!
+        System.out.println(Divide(154,4));
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -13,15 +14,40 @@ public class Main {
         studentFiles.put("Stefanie", "CoolProgram.java");
 
         // Test out your CheckFileExtension() function!
+
+        System.out.println(CheckFileExtension("MyCode"));
     }
 
-    public static void Divide(int x, int y)
+    public static int Divide(int x, int y)
     {
         // Write code here!
+        if (y==0) {
+            try {
+                throw new ArithmeticException("You're trying to divide by 0; please try a different number");
+            } catch (ArithmeticException e) {
+                e.printStackTrace();
+            }
+        }
+        return (x/y);
     }
 
     public static int CheckFileExtension(String fileName)
     {
+        int studentPoints = 0;
+
         // Write code here!
+        if (fileName.endsWith(".java")) {
+            studentPoints = 1;
+        } else if (fileName == null || fileName.isEmpty()) {
+            try {
+                studentPoints = -1;
+                throw new NoFileException("Student has not submitted a file");
+            } catch (NoFileException e) {
+                e.printStackTrace();
+            }
+        } else {
+            studentPoints = 0;
+        }
+        return studentPoints;
     }
 }
---
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/NoFileException.java
@@ -0,0 +1,7 @@
+package org.launchcode;
+
+public class NoFileException extends Exception {
+    public NoFileException(String message) {
+        super(message);
+    }
+}
